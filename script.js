f32df63e29e6d6d6df92df0eaf6670a233bd6299
Ernani Ferreira <43657460+ernaniferreira@users.noreply.github.com>
2024-02-21 20:22:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,27 +19,74 @@ const barbearia = {
   
   
 function buscaCortePorId(id) {
+    for (let i = 0; i < barbearia.cortes.length; i++) {
+        if (barbearia.cortes[i].id === id) {
+            return barbearia.cortes[i];
+        }
+    }
+
+    return "Corte não encontrado";
 }
+
 
 function buscaBarbaPorId(id) {
+    for(let i = 0; i < barbearia.barbas.length; i++){
+        if(barbearia.barbas[i].id === id){
+            return barbearia.barbas[i];
+        }
+    }
+    return "Barba não encontrada";
+
 }
 
-function verificaStatusBarbearia() {
+function verificaStatusBarbearia(){
+    if (barbearia.estaAberto){
+        return "Estamos abertos";
+    }
+    return "Estamos fechados";
+
 }
+
+
 
 function retornaTodosCortes() {
+    return barbearia.cortes;
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas;
 }
 
 function criaPedido(nomeCliente, corteId, barbaId) {
+
+    let corte = buscaCortePorId(corteId);
+    let barba = buscaBarbaPorId(barbaId);
+    const pedido = {
+        nome: nomeCliente,
+        pedidoCorte: corte.tipo,
+        pedidoCortePreco: corte.valor,
+        pedidoBarba: barba.tipo,
+        pedidoBarbaPreco: barba.valor,
+    }
+    return pedido;
+
 }
 
-function atualizarServico(lista, id, valor, tipo) {
+function atualizarServico(lista, id, novoValor, novoTipo) {
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i].id === id) {
+            lista[i].valor = novoValor;
+            lista[i].tipo = novoTipo;
+        }
+    }
+
+    return lista;
 }
 
 function calculaTotal(pedido) {
+    const preco1 = pedido.pedidoCortePreco;
+    const preco2 = pedido.pedidoBarbaPreco;
+    soma = preco1 + preco2;
+    return soma;
 }
-  
-  
+
